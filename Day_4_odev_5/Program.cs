03e773a8c0e6ab8afa929e9364371ffbddd5fde2
YusufDeniz1627 <myusufdeniz8@gmail.com>
2022-01-23 21:34:51 +0300

    a
    a

---
+++ Day_4_odev_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_4_odev_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> names = new List<string>();
+            names.Add("Yusuf");
+            names.Add("Faruk");
+            names.Add("Engin");
+            names.Add("Ayşe");
+            Console.WriteLine(names.Count);
+            
+            MyList<String> name = new MyList<string>();
+            name.add("Yusuf");
+            name.add("Faruk");
+            name.add("Engin");
+            name.add("Ayşe");
+            Console.WriteLine(name.Count);
+            
+        }
+    }
+}
